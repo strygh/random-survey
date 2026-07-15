fcc2456f0609d52398932dd890b156d084d9f8c0
strygh <styang114@smail.nchu.edu.tw>
2026-07-15 18:31:01 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,12 +1,3 @@
-/*
- * 將要隨機派發的網址放入下方（不需排序）
- *
- * 並請注意：
- * 1. 網址請用引號（或稱「撇號」，單引號或雙引號皆可）包起來
- * 2. 包起來的網址之間用逗號分隔
- */
-
-
 const urls = [
 	'https://www.surveycake.com/s/zAWLG',
 	'https://www.surveycake.com/s/adoVr',
