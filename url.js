aea1e3252f1646a4a68928073db378ecb01cf84d
strygh <styang114@smail.nchu.edu.tw>
2026-07-20 15:26:59 +0800

    Update url.js

--- url.js
+++ url.js
@@ -2,5 +2,5 @@ const urls = [
 	'https://www.surveycake.com/s/zAWLG',
 	'https://www.surveycake.com/s/adoVr',
 	'https://www.surveycake.com/s/BeXpy',
-	'https://www.surveycake.com/s/3qKR6',
+	'https://www.surveycake.com/s/3qKR6'
 ];
